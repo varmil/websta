6011b06cfe1fba74d71f298429a75ba62b2cab15
Akihiro Yamamoto <fkmks247@gmail.com>
2016-08-02 17:15:38 +0900

    Update todos.js

--- server/public/js/todos.js
+++ server/public/js/todos.js
@@ -2,9 +2,8 @@
 // [Jérôme Gravel-Niquet](http://jgn.me/). This demo uses a simple
 // [LocalStorage adapter](backbone-localstorage.html)
 // to persist Backbone models within your browser.
-
 // Load the application once the DOM is ready, using `jQuery.ready`:
-$(function(){
+$(function() {
 
 	// Todo Model
 	// ----------
@@ -12,7 +11,7 @@ $(function(){
 	// Our basic **Todo** model has `title`, `url`, and `done` attributes.
 	var Todo = Backbone.Model.extend({
 		// Duration of a model (m seconds)
-		duration: 60*60*24*1000,
+		duration: 60 * 60 * 24 * 1000,
 
 		// Default attributes for the todo item.
 		defaults: function() {
@@ -20,35 +19,42 @@ $(function(){
 				title: "Loading Title...",
 				done: false,
 				url: null,
-				created_at: new Date().toString() ,
+				created_at: new Date().toString(),
 				delete_at: new Date().getTime() + this.duration,
 				archive: false,
 				comment: null,
-				page: Todos.getPage(),	// Archiveページング用の属性
+				page: Todos.getPage(), // Archiveページング用の属性
 			};
 		},
 
 		// Toggle the `done` state of this todo item.
 		toggle: function() {
-			this.save({id: this.id, done: !this.get("done")});
+			this.save({
+				id: this.id,
+				done: !this.get("done")
+			});
 		},
 
-	// 削除日時を過ぎていたら削除。既読ならばArchivesへ。renderする際にskipするか
-	shouldBeSkipped: function() {
-		if (this.get('archive')) return true;
-		var date = new Date().getTime();
-		var delete_at = this.get('delete_at');
-		if (date > delete_at) {
-			if (this.get('done'))
-			{
-				this.save({id: this.id, archive: true, delete_at: -1, done: false});
+		// 削除日時を過ぎていたら削除。既読ならばArchivesへ。renderする際にskipするか
+		shouldBeSkipped: function() {
+			if (this.get('archive')) return true;
+			var date = new Date().getTime();
+			var delete_at = this.get('delete_at');
+			if (date > delete_at) {
+				if (this.get('done')) {
+					this.save({
+						id: this.id,
+						archive: true,
+						delete_at: -1,
+						done: false
+					});
+					return true;
+				}
+				this.destroy();
 				return true;
 			}
-			this.destroy();
-			return true;
-		}
-		return false;
-	},
+			return false;
+		},
 	});
 
 	// Todo Collection
@@ -67,19 +73,25 @@ $(function(){
 
 		// Filter down the list of all todo items that are finished.
 		done: function(archive_flg) {
-			return this.where({done: true, archive: archive_flg});
+			return this.where({
+				done: true,
+				archive: archive_flg
+			});
 		},
 
 		// Filter down the list to only todo items that are still not finished.
 		remaining: function(archive_flg) {
-			return this.where({done: false, archive: archive_flg});
+			return this.where({
+				done: false,
+				archive: archive_flg
+			});
 		},
 
-	// TODO 実装途中
+		// TODO 実装途中
 		getPage: function() {
-		var perPage = 100;
+			var perPage = 100;
 			if (!this.length) return 1;
-		var quotient = Math.floor(this.length / perPage);
+			var quotient = Math.floor(this.length / perPage);
 			return quotient + 1;
 		},
 
@@ -97,21 +109,21 @@ $(function(){
 	var TodoView = Backbone.View.extend({
 
 		//... is a list tag.
-		tagName:  "li",
+		tagName: "li",
 
 		// Cache the template function for a single item.
 		template: _.template($('#item-template').html()),
 
 		// The DOM events specific to an item.
 		events: {
-			"click .toggle"				: "toggleDone",
-			"click .content_text"	: "toggleDone",
-			"dblclick .view"			: "edit",
-			"click a.destroy"			: "clear",
-			"keypress .edit"			: "updateOnEnter",
-			"blur .edit"					: "close",
-			"mouseenter img.thumbnail"	: "startZoom",
-			"mouseleave img.thumbnail"	: "endZoom",
+			"click .toggle": "toggleDone",
+			"click .content_text": "toggleDone",
+			"dblclick .view": "edit",
+			"click a.destroy": "clear",
+			"keypress .edit": "updateOnEnter",
+			"blur .edit": "close",
+			"mouseenter img.thumbnail": "startZoom",
+			"mouseleave img.thumbnail": "endZoom",
 		},
 
 		// The TodoView listens for changes to its model, re-rendering. Since there's
@@ -121,8 +133,8 @@ $(function(){
 			this.listenTo(this.model, 'change', this.render);
 			this.listenTo(this.model, 'destroy', this.remove);
 
-		// Swipeイベントを有効に
-		this.startSwipe();
+			// Swipeイベントを有効に
+			this.startSwipe();
 		},
 
 		// Re-render the titles of the todo item.
@@ -150,7 +162,10 @@ $(function(){
 			if (!value) {
 				this.clear();
 			} else {
-				this.model.save({id: this.model.id, title: value});
+				this.model.save({
+					id: this.model.id,
+					title: value
+				});
 				this.$el.removeClass("editing");
 			}
 		},
@@ -164,69 +179,67 @@ $(function(){
 		// Remove the item, destroy the model.
 		clear: function() {
 			var _this = this;
-			this.$el.hide('slow', function(){
+			this.$el.hide('slow', function() {
 				_this.model.destroy();
 				// TODO HTTP DELETE method to express.
 			});
 		},
 
-	// Swipeの制御
-	startSwipe: function() {
-		var _this = this;
-		var threshold = 350;
-		this.$el.swipe( {
-			swipeStatus:function(event, phase, direction, distance, duration, fingerCount)
-			{
-				var str = "";
-				if (phase=="move") {
-					if (direction =="left") {
-						$(this).css("-webkit-transform", "translateX(-"+ distance +"px)");
-						$(this).css("-moz-transform", "translateX(-"+ distance +"px)");
-						$(this).css("-o-transform", "translateX(-"+ distance +"px)");
-						$(this).css("-ms-transform", "translateX(-"+ distance +"px)");
-						$(this).css("opacity", 1.0 - 1.0 * distance / threshold);
+		// Swipeの制御
+		startSwipe: function() {
+			var _this = this;
+			var threshold = 350;
+			this.$el.swipe({
+				swipeStatus: function(event, phase, direction, distance, duration, fingerCount) {
+					var str = "";
+					if (phase == "move") {
+						if (direction == "left") {
+							$(this).css("-webkit-transform", "translateX(-" + distance + "px)");
+							$(this).css("-moz-transform", "translateX(-" + distance + "px)");
+							$(this).css("-o-transform", "translateX(-" + distance + "px)");
+							$(this).css("-ms-transform", "translateX(-" + distance + "px)");
+							$(this).css("opacity", 1.0 - 1.0 * distance / threshold);
+						} else if (direction == "right") {
+							$(this).css("-webkit-transform", "translateX(" + distance + "px)");
+							$(this).css("-moz-transform", "translateX(" + distance + "px)");
+							$(this).css("-o-transform", "translateX(" + distance + "px)");
+							$(this).css("-ms-transform", "translateX(" + distance + "px)");
+							$(this).css("opacity", 1.0 - 1.0 * distance / threshold);
+						}
+					} else if (phase == "cancel") {
+						// スワイプが足りないので元に戻す
+						$(this).css("-webkit-transform", "translateX(" + 0 + "px)");
+						$(this).css("-moz-transform", "translateX(" + 0 + "px)");
+						$(this).css("-o-transform", "translateX(" + 0 + "px)");
+						$(this).css("-ms-transform", "translateX(" + 0 + "px)");
+						$(this).css("opacity", 1.0);
+					} else if (phase == "end") {
+						// 一定以上スワイプしたので削除
+						_this.clear();
 					}
-					else if (direction =="right") {
-						$(this).css("-webkit-transform", "translateX("+ distance +"px)");
-						$(this).css("-moz-transform", "translateX("+ distance +"px)");
-						$(this).css("-o-transform", "translateX("+ distance +"px)");
-						$(this).css("-ms-transform", "translateX("+ distance +"px)");
-						$(this).css("opacity", 1.0 - 1.0 * distance / threshold);
-					}
-				} else if (phase =="cancel") {
-					// スワイプが足りないので元に戻す
-					$(this).css("-webkit-transform", "translateX("+ 0 +"px)");
-					$(this).css("-moz-transform", "translateX("+ 0 +"px)");
-					$(this).css("-o-transform", "translateX("+ 0 +"px)");
-					$(this).css("-ms-transform", "translateX("+ 0 +"px)");
-					$(this).css("opacity", 1.0);
-				} else if (phase =="end") {
-					// 一定以上スワイプしたので削除
-					_this.clear();
-				}
-			},
-			threshold: threshold,
-			// maxTimeThreshold:2500,
-			excludedElements:"button, input, select, textarea, .noSwipe, a",
-			triggerOnTouchEnd:false,	// true:指を離すまで発火しない
-			allowPageScroll:"auto",
-		});
-	},
+				},
+				threshold: threshold,
+				// maxTimeThreshold:2500,
+				excludedElements: "button, input, select, textarea, .noSwipe, a",
+				triggerOnTouchEnd: false, // true:指を離すまで発火しない
+				allowPageScroll: "auto",
+			});
+		},
 
-	startZoom: function(e) {
-		var zoom_width = "350px";
-		$(e.target).stop().animate({
-					'width': zoom_width,//拡大で表示させておくサイズ
-					// 'marginTop':'245px'//トップのマージンをマイナスで指定する事で底辺を起点としています
-				},'fast');
-	},
-	endZoom: function(e) {
-		var default_widht = "85px";
-		$(e.target).stop().animate({
-					'width': default_widht,//デフォルトで表示させておくサイズ
-					// 'marginTop':'0px'
-				},'fast');
-	},
+		startZoom: function(e) {
+			var zoom_width = "350px";
+			$(e.target).stop().animate({
+				'width': zoom_width, //拡大で表示させておくサイズ
+				// 'marginTop':'245px'//トップのマージンをマイナスで指定する事で底辺を起点としています
+			}, 'fast');
+		},
+		endZoom: function(e) {
+			var default_widht = "85px";
+			$(e.target).stop().animate({
+				'width': default_widht, //デフォルトで表示させておくサイズ
+				// 'marginTop':'0px'
+			}, 'fast');
+		},
 	});
 
 	// The Application
@@ -235,7 +248,7 @@ $(function(){
 	// Our overall **AppView** is the top-level piece of UI.
 	var AppView = Backbone.View.extend({
 		// page: 1,	// ページング用変数
-		isArchive: false,	// 現在の表示がアーカイブか。
+		isArchive: false, // 現在の表示がアーカイブか。
 
 		// Instead of generating a new element, bind to the existing skeleton of
 		// the App already present in the HTML.
@@ -246,7 +259,7 @@ $(function(){
 
 		// Delegated events for creating new items, and clearing completed ones.
 		events: {
-			"keypress #new-todo":  "createOnEnter",
+			"keypress #new-todo": "createOnEnter",
 			"click #clear-completed": "clearCompleted",
 			"click #toggle-archives": "toggleArchives",
 			"scroll": "scroll",
@@ -261,10 +274,10 @@ $(function(){
 
 			this.input = this.$("#new-todo");
 
-			this.listenTo(Todos, 'reset', this.loadWebs);	// page reload時に一度だけ発火
-			this.listenTo(Todos, 'add', this.addOne);			// collection.create()時に発火
-			this.listenTo(Todos, 'change', this.render);		// collectionはmodelが発火したイベントも受け取れる
-			this.listenTo(Todos, 'destroy', this.render);		// modelが破棄されたら発火
+			this.listenTo(Todos, 'reset', this.loadWebs); // page reload時に一度だけ発火
+			this.listenTo(Todos, 'add', this.addOne); // collection.create()時に発火
+			this.listenTo(Todos, 'change', this.render); // collectionはmodelが発火したイベントも受け取れる
+			this.listenTo(Todos, 'destroy', this.render); // modelが破棄されたら発火
 
 			this.on('getTitle', this.getTitle);
 			this.on('getComment', this.getComment);
@@ -272,7 +285,9 @@ $(function(){
 			this.status_bar = this.$('.status_bar');
 			this.main = $('#main');
 
-			Todos.fetch({reset: true});  // reset: true にするとaddが発火しなくなり、reset: falseにするとaddが発火する
+			Todos.fetch({
+				reset: true
+			}); // reset: true にするとaddが発火しなくなり、reset: falseにするとaddが発火する
 		},
 
 		// Re-rendering the App just means refreshing the statistics -- the rest
@@ -283,7 +298,10 @@ $(function(){
 
 			if (Todos.length) {
 				this.main.show();
-				this.status_bar.html(this.statsTemplate({done: done, remaining: remaining}));
+				this.status_bar.html(this.statsTemplate({
+					done: done,
+					remaining: remaining
+				}));
 			} else {
 				this.main.hide();
 			}
@@ -316,17 +334,25 @@ $(function(){
 
 			$.when(titleDeferred, commentDeferred).done(function() {
 				// PUT要求を送る（タイトルとコメント両方がとれたタイミングで）
-				todo.save({ id: todo.get('id'), title: title, comment: comment });
+				todo.save({
+					id: todo.get('id'),
+					title: title,
+					comment: comment
+				});
 			});
 
-			if (this.isArchive) return;	// アーカイブ表示時はprependしない
-			var view = new TodoView({model: todo});
+			if (this.isArchive) return; // アーカイブ表示時はprependしない
+			var view = new TodoView({
+				model: todo
+			});
 			this.$("#todo-list").prepend(view.render().el);
 		},
 
-	// ページ読み込み時のロード
+		// ページ読み込み時のロード
 		loadOne: function(todo) {
-			var view = new TodoView({model: todo});
+			var view = new TodoView({
+				model: todo
+			});
 			this.$("#todo-list").prepend(view.render().el);
 		},
 
@@ -338,12 +364,12 @@ $(function(){
 			};
 			var res = Todos.where(attributes);
 			console.debug('res = ', res);
-			for(var i=0; i<res.length; i++) {
+			for (var i = 0; i < res.length; i++) {
 				this.flg = res[i].shouldBeSkipped();
 				if (this.flg) continue;
 				this.loadOne(res[i]);
 			}
-			this.render();  // kick render()
+			this.render(); // kick render()
 		},
 
 		// for Archive loading
@@ -355,7 +381,7 @@ $(function(){
 			};
 			var res = Todos.where(attributes);
 			// this.page++;
-			for(var i=0; i<res.length; i++) {
+			for (var i = 0; i < res.length; i++) {
 				this.loadOne(res[i]);
 			}
 			this.render();
@@ -368,7 +394,11 @@ $(function(){
 			if (!this.input.val()) return;
 
 			// addイベントの発火タイミングを遅らせる（サーバーの応答後にする）
-			Todos.create({url: this.input.val()}, { wait: true });
+			Todos.create({
+				url: this.input.val()
+			}, {
+				wait: true
+			});
 			this.input.val('');
 		},
 
@@ -379,16 +409,16 @@ $(function(){
 		},
 
 		// Archives表示と通常表示とを切り替える
-		toggleArchives: function (e) {
-			this.$("#todo-list").html('');	//HTMLを一度空にする
+		toggleArchives: function(e) {
+			this.$("#todo-list").html(''); //HTMLを一度空にする
 			this.$("#toggle-archives").toggleClass("selected");
 			// this.page = 1;	//page 初期化
 			this.isArchive = !this.isArchive;
-			if (this.isArchive){
+			if (this.isArchive) {
 				this.loadArchives();
 			} else {
 				this.loadWebs();
-		}
+			}
 		},
 
 		// Scroll event
@@ -397,40 +427,38 @@ $(function(){
 			var contentsHeight = $(document).height();
 			var scrollPosition = $(window).height() + $(window).scrollTop();
 			var threshold = 0;
-			if (contentsHeight - scrollPosition < threshold)
-			{
+			if (contentsHeight - scrollPosition < threshold) {
 				console.debug('scrolled to bottom');
 			}
 
 		},
 
-	// modelのurlを元にタイトルを取得する。
-	// API: http://ryouchi.seesaa.net/article/42059413.html
-	getTitle: function(model, cb) {
-		$.getJSON('http://www.usamimi.info/~ryouchi/title/get_title_jsonp.php?callback=?',
-		{
-			url: model.get('url'),
+		// modelのurlを元にタイトルを取得する。
+		// API: http://ryouchi.seesaa.net/article/42059413.html
+		getTitle: function(model, cb) {
+			$.getJSON('http://www.usamimi.info/~ryouchi/title/get_title_jsonp.php?callback=?', {
+					url: model.get('url'),
+				},
+				function(res) {
+					cb(res.title);
+				});
 		},
-		function(res){
-			cb(res.title);
-		});
-	},
 
-	// スクレイピング
-	getComment: function(model, cb) {
-		var str_length = 180;
-		$.ajax({
-			url: model.get('url'),
-			type: 'GET',
-			success: function(res) {
-				// スクレイピングしたcontentsから特定のタグの内容を切り出す。
-				var str = $(res.responseText).find('h1, h2, h3, p').text();
-				// 文字列の切り出し
-				result_str = str.substring(0, str_length);
-				cb(result_str);
-			}
-		});
-	},
+		// スクレイピング
+		getComment: function(model, cb) {
+			var str_length = 180;
+			$.ajax({
+				url: model.get('url'),
+				type: 'GET',
+				success: function(res) {
+					// スクレイピングしたcontentsから特定のタグの内容を切り出す。
+					var str = $(res.responseText).find('h1, h2, h3, p').text();
+					// 文字列の切り出し
+					result_str = str.substring(0, str_length);
+					cb(result_str);
+				}
+			});
+		}
 	});
 
 	// Finally, we kick things off by creating the **App**.
